622766c35a63c8beb8eebedcd768f52cbfc4d624
Мето Трајковски <metot@hotmail.com>
2019-02-11 18:19:22 +0100

    Created separate object for the materialize components

--- src/js/popup.js
+++ src/js/popup.js
@@ -1,65 +1,68 @@
-var app = new Vue({
+MaterializeComponents = {
+    tabsInstance: undefined,
+    activeInstance: undefined,
+    archiveInstance: undefined,
+    floatingButtonInstance: undefined,
+    refreshIntervalInstance: undefined,
+    addModal: {
+        addModalInstance: undefined,
+        trackingNumberInput: undefined,
+        trackingNumberLabel: undefined,
+        packageDescriptionInput: undefined,
+        packageDescriptionLabel: undefined
+    }
+};
+
+var vueApp = new Vue({
     el: '#app',
     data: {
-        tabsInstance: undefined,
-        activeInstance: undefined,
-        archiveInstance: undefined,
-        floatingButtonInstance: undefined,
-        refreshIntervalInstance: undefined,
-        addModalInstance: undefined,
-        trackingNumber: "",
-        packageDescription: "",
+        addNewPackage: {
+            trackingNumber: "",
+            packageDescription: "",
+        },
         activePackages: [],
         archivePackages: []
     },
     mounted: function () {
-        this.$nextTick(function () { // init materialize components
+        this.$nextTick(function () {
+            /* init materialize components */
+
             // tabs
-            this.tabsInstance = M.Tabs.init(this.$el.querySelector("#tabs"));
-            this.tabsInstance.select("activeView");
+            MaterializeComponents.tabsInstance = M.Tabs.init(this.$el.querySelector("#tabs"));
+            MaterializeComponents.tabsInstance.select("activeView");
+
             // collapsible
-            this.activeInstance = M.Collapsible.init(this.$el.querySelector("#activeCollapsible"));
-            this.archiveInstance = M.Collapsible.init(this.$el.querySelector("#archiveCollapsible"));
+            MaterializeComponents.activeInstance = M.Collapsible.init(this.$el.querySelector("#activeCollapsible"));
+            MaterializeComponents.archiveInstance = M.Collapsible.init(this.$el.querySelector("#archiveCollapsible"));
+
             // floating button
-            this.floatingButtonInstance = M.FloatingActionButton.init(this.$el.querySelector("#floatingButton"));
-            // refresh interval
-            this.refreshIntervalInstance = M.FormSelect.init(this.$el.querySelector("#refreshInterval"));
+            MaterializeComponents.floatingButtonInstance = M.FloatingActionButton.init(this.$el.querySelector("#floatingButton"));
+
+            // settings view
+            MaterializeComponents.refreshIntervalInstance = M.FormSelect.init(this.$el.querySelector("#refreshInterval"));
+
             // add package modal
-            this.addModalInstance = M.Modal.init(this.$el.querySelector("#addModal"));
+            MaterializeComponents.addModal.addModalInstance = M.Modal.init(this.$el.querySelector("#addModal"), {
+                dismissible: false
+            });
+            MaterializeComponents.addModal.trackingNumberInput = this.$el.querySelector("#tracking_number");
+            MaterializeComponents.addModal.trackingNumberLabel = this.$el.querySelector("#tracking_number_label");
+            MaterializeComponents.addModal.packageDescriptionInput = this.$el.querySelector("#package_description");
+            MaterializeComponents.addModal.packageDescriptionLabel = this.$el.querySelector("#package_description_label");
         })
     },
     methods: {
         deleteActivePackage: function (index) {
-            this.activeInstance.options.outDuration = 0;
-            this.activeInstance.close(index);
-            this.activeInstance.options.outDuration = 300;
+            MaterializeComponents.activeInstance.options.outDuration = 0;
+            MaterializeComponents.activeInstance.close(index);
+            MaterializeComponents.activeInstance.options.outDuration = 300;
             this.activePackages.splice(index, 1);
         },
         deleteArchivePackage: function (index) {
-            this.archiveInstance.options.outDuration = 0;
-            this.archiveInstance.close(index);
-            this.archiveInstance.options.outDuration = 300;
+            MaterializeComponents.archiveInstance.options.outDuration = 0;
+            MaterializeComponents.archiveInstance.close(index);
+            MaterializeComponents.archiveInstance.options.outDuration = 300;
             this.archivePackages.splice(index, 1);
-        },
-        addNewActivePackage: function () {
-            this.activePackages.push({
-                trackingNumber: this.trackingNumber,
-                packageDescription: this.packageDescription,
-                status: "local_shipping",
-                notifications: 3
-            });
-
-            this.trackingNumber = "";
-            this.packageDescription = "";
-            
-            // update input field
-            this.$el.querySelector("#tracking_number").classList.remove("valid");
-            this.$el.querySelector("#tracking_number").classList.remove("invalid");
-            this.$el.querySelector("#tracking_number_label").classList.remove("active");
-
-            this.$el.querySelector("#package_description").classList.remove("valid");
-            this.$el.querySelector("#package_description").classList.remove("invalid");
-            this.$el.querySelector("#package_description_label").classList.remove("active");
         },
         archiveActivePackage: function (index) {
             this.archivePackages.push(this.activePackages[index]);
@@ -68,6 +71,36 @@ var app = new Vue({
         activeArchivePackage: function (index) {
             this.activePackages.push(this.archivePackages[index]);
             this.deleteArchivePackage(index);
+        },
+        openAddModal: function () {
+            // update input fields
+            this.addNewPackage.trackingNumber = "";
+            this.addNewPackage.packageDescription = "";
+
+            MaterializeComponents.addModal.trackingNumberInput.classList.remove("valid");
+            MaterializeComponents.addModal.trackingNumberInput.classList.remove("invalid");
+            MaterializeComponents.addModal.trackingNumberLabel.classList.remove("active");
+
+            MaterializeComponents.addModal.packageDescriptionInput.classList.remove("valid");
+            MaterializeComponents.addModal.packageDescriptionInput.classList.remove("invalid");
+            MaterializeComponents.addModal.packageDescriptionLabel.classList.remove("active");
+
+            // open modal
+            MaterializeComponents.addModal.addModalInstance.open();
+
+            // focus on first field
+            MaterializeComponents.addModal.trackingNumberInput.focus();
+        },
+        addNewActivePackage: function () {
+            this.activePackages.push({
+                trackingNumber: this.addNewPackage.trackingNumber,
+                packageDescription: this.addNewPackage.packageDescription,
+                status: "local_shipping",
+                notifications: 3
+            });
+
+            // close modal
+            MaterializeComponents.addModal.addModalInstance.close();
         }
     }
 });
