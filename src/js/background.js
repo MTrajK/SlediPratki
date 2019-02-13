e49059c0ebf2ab22f75d407bbbfebe1613527cd7
Мето Трајковски <metot@hotmail.com>
2019-02-13 21:28:40 +0100

    Start with background

---
+++ src/js/background.js
@@ -0,0 +1,3 @@
+(function (){
+    Common.startBackground();
+})();
--- src/js/common.js
+++ src/js/common.js
@@ -1,35 +1,58 @@
 Common = (function () {
 
     var postUrl = "https://www.posta.com.mk/tnt/api/query?id=";
+    var maxRequestTime = 15000; // 15 seconds max request
 
-    function convertDate(date) {
-        return date.getDate() + "."
-            + (date.getMonth() + 1) + "."
+    var addZero = function(num) {
+        return (num < 10 ? "0" : "") + num;
+    }
+
+    var convertDate = function(date) {
+        return addZero(date.getDate()) + "."
+            + addZero(date.getMonth() + 1) + "."
             + date.getFullYear() + ", "
-            + date.getHours() + ":"
-            + date.getMinutes() + ":"
-            + date.getSeconds();
+            + addZero(date.getHours()) + ":"
+            + addZero(date.getMinutes()) + ":"
+            + addZero(date.getSeconds());
     };
 
-    function getDateTime() {
+    var getDateTime = function() {
         return convertDate(new Date());
     };
 
-    function getPackage(trackingNumber, end) {
+    var getPackage = function (trackingNumber, end) {
         axios({
             method: 'get',
             url: postUrl + trackingNumber,
-            timeout: 15000 // 15 seconds max request
+            timeout: maxRequestTime
         }).then(function (response) {
             end(response);
         }).catch(function (error) {
             console.log(error);
             end("error");
         });
-    }
+    };
+
+    var startBackground = function() {
+        /*
+        var lastRefresh = getLastRefresh();
+        var refreshInterval = getRefreshInterval();
+    
+        if (lastRefresh >= refreshInterval) {
+            // set interval
+        } else {
+            // set timeout and inside set interval
+        }
+        // stop the interval in this way
+        var refreshIntervalId = setInterval(fname, 10000);
+        
+        clearInterval(refreshIntervalId);
+        */
+    };
 
     return {
         getDateTime: getDateTime,
         getPackage: getPackage,
+        startBackground: startBackground
     };
 })();
