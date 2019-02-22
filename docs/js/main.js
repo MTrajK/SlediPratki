f8b2419bdcdc2badbb332864a4b913f578327e2d
Мето Трајковски <metot@hotmail.com>
2019-02-22 23:52:50 +0100

    Start with the web page

---
+++ docs/js/main.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // wait for the DOM to be created (maybe we don't need this tag https://thanpol.as/javascript/you-dont-need-dom-ready )
+    // define materialize and logic
+
+});
+
+window.addEventListener("load", function(event) {
+    // wait for all pictures to be loaded
+    // stop the main spinner
+
+});
