0399e3494d706322eefd23c1f5a83755371f1d59
Мето Трајковски <metot@hotmail.com>
2019-02-23 13:14:53 +0100

    Added navbar with scrollspy

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,10 +1,11 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     // wait for the DOM to be created (maybe we don't need this tag https://thanpol.as/javascript/you-dont-need-dom-ready )
     // define materialize and logic
-
+    var elems = document.querySelectorAll('.scrollspy');
+    var instances = M.ScrollSpy.init(elems);
 });
 
-window.addEventListener("load", function(event) {
+window.addEventListener("load", function (event) {
     // wait for all pictures to be loaded
     // stop the main spinner
 
